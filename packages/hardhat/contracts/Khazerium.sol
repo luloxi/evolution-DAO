abf29ddd231c86e48bfbba2e351891a8ee4117bb
scobru <scobru1988@gmail.com>
2023-05-01 00:20:35 +0200

    ERC20Votes implementation on Khazerium.sol

--- packages/hardhat/contracts/Khazerium.sol
+++ packages/hardhat/contracts/Khazerium.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 enum Option {
