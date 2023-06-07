4af0c92d272c265fbe9357042ffcca714c9bb51a
Lulox <lulox.eth@protonmail.com>
2023-06-07 01:02:42 -0300

    Removed proposal creation from deploy script

--- packages/hardhat/deploy/13_deploy_khazum.js
+++ packages/hardhat/deploy/13_deploy_khazum.js
@@ -1,6 +1,6 @@
 // deploy/01_deploy_khazum.js
 
-const { ethers } = require("hardhat");
+// const { ethers } = require("hardhat");
 
 const localChainId = "31337";
 
@@ -18,26 +18,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
     waitConfirmations: chainId === localChainId ? 0 : 5,
   });
-
-  const khazum = await ethers.getContract("Khazum", deployer);
-
-  // First proposal parameters
-  const title = "Who is your favorite?"; // i.e: Best ice-cream flavor
-  const description = "Alice or Bob?"; // Option A is Chocolate, Option B is Strawberry
-  const proposalDurationInMinutes = "3"; // Duration of the proposal in minutes
-  const minimumVotes = "5"; // Minimum amount of votes the proposal should receive to declare a winner
-
-  // Create a sample proposal if on localhost
-  if (chainId == "31337") {
-    await khazum.createProposal(
-      title,
-      description,
-      proposalDurationInMinutes,
-      minimumVotes
-    );
-  }
-
-  console.log("Proposal created successfully!");
 
   try {
     // Verify contract if not on local chain
