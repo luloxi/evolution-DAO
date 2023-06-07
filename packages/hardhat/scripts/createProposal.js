b5b762d1dc8d590d062fee89810bd3a008dfa476
Lulox <lulox.eth@protonmail.com>
2023-06-07 17:08:49 -0300

    Contract and createProposal.js solution working, frontend missing changes

--- packages/hardhat/scripts/createProposal.js
+++ packages/hardhat/scripts/createProposal.js
@@ -4,7 +4,7 @@ const khazumContractAddress =
 
 async function main() {
   // Checkpoint 3: Create a const called minimumVotes and set it to the amount of votes the proposal should receive to declare a winner
-  // Checkpoint 4: Create a const for each option name and address
+  // Checkpoint 4: Create a const for each option address and name
   /* Setting up the proposal details */
   const title =
     "Who's the next donation receiver? (A: Save the cats | B: Save the dogs)"; // Title of your proposal
@@ -21,6 +21,7 @@ async function main() {
   const khazum = await Khazum.attach(contractAddress);
 
   // Checkpoint 3: Add minimumVotes as an argument to the createProposal call
+  // Checkpoint 4: Add option address and names as arguments to the createProposal call
   /* Calling functions on the Khazum contract instance */
   await khazum.createProposal(
     title,
